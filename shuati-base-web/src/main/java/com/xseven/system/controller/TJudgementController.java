8745cb21daf0bd2a861cb5afb11f3d0f25c9d7f6
gloxxxxi <1169550631@qq.com>
2021-01-28 15:26:13 +0800

    测试分支

---
+++ shuati-base-web/src/main/java/com/xseven/system/controller/TJudgementController.java
@@ -0,0 +1,11 @@
+package com.xseven.system.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value = "判断题",tags = "判断题")
+@RestController
+@RequestMapping("/t-judgement")
+public class TJudgementController {
+}
